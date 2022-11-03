1c08350ee77000b9b28373947178db0de93c3f36
user <user@DESKTOP-LGJCI10>
2022-11-03 09:27:00 +0900

    ?

--- src/main/java/com/wacha/model/MovieDAO.java
+++ src/main/java/com/wacha/model/MovieDAO.java
@@ -621,5 +621,14 @@ public class MovieDAO {
 			
 			return list;
 		}
+		
+		public int deleteMovie(int num) {
+			int result=0;
+			
+			openConn();
+			
+			sql="delete from movie where movie_num";
+		}
+		
 }
 

c93d8766b7c0036dcbe912db767bad915756d2d3
DuBori <92709439+DuBori@users.noreply.github.com>
2022-11-09 16:32:31 +0900

    hihi

--- src/main/java/com/wacha/content/action/WachaSingoUpdateAction.java
+++ src/main/java/com/wacha/content/action/WachaSingoUpdateAction.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.wacha.controller.Action;
 import com.wacha.controller.ActionForward;
@@ -19,9 +20,12 @@ public class WachaSingoUpdateAction implements Action {
 		int coment_num = Integer.parseInt(request.getParameter("coment_num"));
 		int moive_num = Integer.parseInt(request.getParameter("movie_num"));
 		String coment_id = request.getParameter("coment_id");
+		HttpSession session = request.getSession();
+		
+		String sessionId = (String)session.getAttribute("session_id");
 		
 		SingoDAO singo_dao = SingoDAO.getInstance();
-		singo_dao.updateSingo(member_id);
+		singo_dao.updateSingo(member_id,sessionId);
 		
 		ActionForward forward = new ActionForward();
 		forward.setPath("wacha_coment.do?coment_num="+coment_num+"&movie_num="+moive_num+"&session_id="+coment_id);
